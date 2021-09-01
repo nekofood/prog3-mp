3d878cb4774489c53bca2e89ff38d68b4a987c45
dale <sonicroblox@gmail.com>
2021-09-01 15:37:03 +0800

    tile init

--- Board.java
+++ Board.java
@@ -1,6 +1,6 @@
 class Board {
 	private ArrayList<Piece> pieces;
-	private Tile[] terrain;
+	private Tile[] terrain[20];
 
 	Board() {
 		//all piece initiation goes here
@@ -24,6 +24,31 @@ class Board {
 		pieces.add(new Piece("Cat", 8, 2, 2, 2, this));
 		pieces.add(new LionTigerPiece("Tiger", 9, 1, 2, 6, this));
 		pieces.add(new ElephantPiece(7, 1, 2, this));
+
+		//player tiles
+		terrain[0] = new Tile("Den", 1, 4, 1, this);
+		terrain[1] = new Tile("Den", 9, 4, 2, this);
+
+		terrain[2] = new Tile("Trap", 1, 3, 1, this);
+		terrain[3] = new Tile("Trap", 1, 5, 1, this);
+		terrain[4] = new Tile("Trap", 2, 4, 1, this);
+
+		terrain[5] = new Tile("Trap", 9, 3, 2, this);
+		terrain[6] = new Tile("Trap", 9, 5, 2, this);
+		terrain[7] = new Tile("Trap", 8, 4, 2, this);
+
+		terrain[8] = new Tile("Water", 4, 6, 0, this);
+		terrain[9] = new Tile("Water", 5, 6, 0, this);
+		terrain[10] = new Tile("Water", 6, 6, 0, this);
+		terrain[11] = new Tile("Water", 4, 5, 0, this);
+		terrain[12] = new Tile("Water", 5, 5, 0, this);
+		terrain[13] = new Tile("Water", 6, 5, 0, this);
+		terrain[14] = new Tile("Water", 4, 3, 0, this);
+		terrain[15] = new Tile("Water", 5, 3, 0, this);
+		terrain[16] = new Tile("Water", 6, 3, 0, this);
+		terrain[17] = new Tile("Water", 4, 2, 0, this);
+		terrain[18] = new Tile("Water", 5, 2, 0, this);
+		terrain[19] = new Tile("Water", 6, 2, 0, this);
 	}
 
 	public void removePiece(Piece piece) {
