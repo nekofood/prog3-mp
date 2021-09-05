bc4e45c85aec40a6c9f9f13da2e6e247ecc5ad22
dale <sonicroblox@gmail.com>
2021-09-05 17:24:50 +0800

    added den win condition check

--- Board.java
+++ Board.java
@@ -19,9 +19,9 @@ class Board {
 		terrain[5] = new Tile("Trap", 9, 3, 2, this);
 		terrain[6] = new Tile("Trap", 9, 5, 2, this);
 		terrain[7] = new Tile("Trap", 8, 4, 2, this);
-		
+
 		//IDEA: Board() can accept an array containing the positions of water tiles
-		//for customizability and modularity 
+		//for customizability and modularity
 		terrain[8] = new Tile("Water", 4, 6, 0, this);
 		terrain[9] = new Tile("Water", 5, 6, 0, this);
 		terrain[10] = new Tile("Water", 6, 6, 0, this);
@@ -40,12 +40,12 @@ class Board {
 		//left-side pieces (0-7)
 		pieces.add(new LionTigerPiece("Lion", 1, 1, 1, 7, this));
 		pieces.add(new Piece("Dog", 2, 2, 1, 4, this));
-	      	pieces.add(new Piece("Leopard", 3, 3, 1, 5, this)); 	
+	  pieces.add(new Piece("Leopard", 3, 3, 1, 5, this));
 		pieces.add(new MousePiece(3, 1, 1, this));
 		pieces.add(new Piece("Wolf", 3, 6, 1, 3, this));
 		pieces.add(new Piece("Cat", 2, 6, 1, 2, this));
 		pieces.add(new LionTigerPiece("Tiger", 1, 7, 1, 6, this));
-	       	pieces.add(new ElephantPiece(3, 7, 1, this));
+	  pieces.add(new ElephantPiece(3, 7, 1, this));
 		//right side pieces (8-15)
 		pieces.add(new LionTigerPiece("Lion", 9, 7, 2, 7, this));
 		pieces.add(new Piece("Dog", 8, 6, 2, 4, this));
@@ -55,12 +55,29 @@ class Board {
 		pieces.add(new Piece("Cat", 8, 2, 2, 2, this));
 		pieces.add(new LionTigerPiece("Tiger", 9, 1, 2, 6, this));
 		pieces.add(new ElephantPiece(7, 1, 2, this));
-
-
 	}
 
 	public void removePiece(Piece piece) {
 		pieces.remove(piece);
+	}
+
+	/**
+	 * Checks if any player has achieved the "den" win condition.
+	 * @return the number of the player that won (left or right), 0 otherwise
+	 */
+	public int checkEnd() {
+		for (int i=0; i < pieces.size(); i++) {
+			//p1's den check
+			Piece current = pieces.get(i);
+			if (current.getOwner() == 1 && current.getX() == terrain[1].getX() && current.getY() == terrain[1].getY()) {
+					return 1;
+			}
+			//p2's den check
+			if (current.getOwner() == 2 && current.getX() == terrain[0].getX() && current.getY() == terrain[0].getY()) {
+					return 2;
+			}
+
+			return 0;
 	}
 
 	public Piece[] getBoard() {
