f96753b648716eafd09e1e642bddc9fc2636822a
dale <sonicroblox@gmail.com>
2021-09-06 17:08:49 +0800

    indent

--- Board.java
+++ Board.java
@@ -40,12 +40,12 @@ class Board {
 		//left-side pieces (0-7)
 		pieces.add(new LionTigerPiece("Lion", 1, 1, 1, 7, this));
 		pieces.add(new Piece("Dog", 2, 2, 1, 4, this));
-	  pieces.add(new Piece("Leopard", 3, 3, 1, 5, this));
+	  	pieces.add(new Piece("Leopard", 3, 3, 1, 5, this));
 		pieces.add(new MousePiece(3, 1, 1, this));
 		pieces.add(new Piece("Wolf", 3, 6, 1, 3, this));
 		pieces.add(new Piece("Cat", 2, 6, 1, 2, this));
 		pieces.add(new LionTigerPiece("Tiger", 1, 7, 1, 6, this));
-	  pieces.add(new ElephantPiece(3, 7, 1, this));
+	  	pieces.add(new ElephantPiece(3, 7, 1, this));
 		//right side pieces (8-15)
 		pieces.add(new LionTigerPiece("Lion", 9, 7, 2, 7, this));
 		pieces.add(new Piece("Dog", 8, 6, 2, 4, this));
