6b59b6fd38d7f6eba6ad6c207a2b46e3b2ee0d4b
nathbinalla <89829147+nathbinalla@users.noreply.github.com>
2021-09-06 15:37:11 +0800

    Update GameView.java

--- GameView.java
+++ GameView.java
@@ -1,5 +1,7 @@
-import java.swing.*;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import javax.swing.event.*;
 
 class GameView extends JFrame {
 	private JButton[][] gameBoard;
